f09681006ea4a5e39f959f901020f73a0c915a45
moyumo <>
2025-06-26 00:59:00 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,12 @@ module.exports = {
     'vue-global-api'
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
   },
   globals: {
     defineProps: 'readonly',
